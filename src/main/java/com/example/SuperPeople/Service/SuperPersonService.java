83e45c6284d1b3a3fd2d81bdc0d91b5811eb2e56
Jordan Suber <jordansuber@gmail.com>
2021-09-29 08:33:30 -0400

    Adds SuperPerson Service

---
+++ src/main/java/com/example/SuperPeople/Service/SuperPersonService.java
@@ -0,0 +1,38 @@
+package com.example.SuperPeople.Service;
+
+import com.example.SuperPeople.DAO.SuperPersonRepository;
+import com.example.SuperPeople.Entity.SuperPerson;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SuperPersonService {
+    private final SuperPersonRepository superPersonRepository;
+
+    public SuperPersonService(SuperPersonRepository superPersonRepository) {
+        this.superPersonRepository = superPersonRepository;
+    }
+
+    public List<SuperPerson> getAllSuperPeople(){
+        return superPersonRepository.findAll();
+    }
+
+    public Object findById(String id){
+        return superPersonRepository.findById(id);
+    }
+
+    public Object findByName(String name){
+        return superPersonRepository.findPersonByName(name);
+    }
+
+    public void deleteById(String id){
+        superPersonRepository.deleteById(id);
+    }
+
+    public void save(SuperPerson superPerson){
+        superPersonRepository.save(superPerson);
+    }
+
+
+}
