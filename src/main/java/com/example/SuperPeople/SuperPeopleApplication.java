eea487146b30b25f0c4100842347ee47afabd41e
Jordan Suber <jordansuber@gmail.com>
2021-09-29 08:05:42 -0400

    first commit

---
+++ src/main/java/com/example/SuperPeople/SuperPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.example.SuperPeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperPeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SuperPeople/SuperPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SuperPeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
